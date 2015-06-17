c26acba2db45ebd40b29816bf2f596bc7efe4c2f
Rafeee <1234QWERasdf$>
2015-06-17 15:20:28 +0200

    old version merged

--- app/src/main/java/headfirstandroiddevelopment/budgeteer/MainActivity.java
+++ app/src/main/java/headfirstandroiddevelopment/budgeteer/MainActivity.java
@@ -1,9 +1,12 @@
 package headfirstandroiddevelopment.budgeteer;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,33 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*show icons*/
+        ImageButton icon1 = (ImageButton) findViewById(R.id.icon1);
+        icon1.setImageResource(R.drawable.apartment);
+        ImageButton icon2 = (ImageButton) findViewById(R.id.icon2);
+        icon2.setImageResource(R.drawable.phone);
+        ImageButton icon3 = (ImageButton) findViewById(R.id.icon3);
+        icon3.setImageResource(R.drawable.shoppingbasket);
+        ImageButton icon4 = (ImageButton) findViewById(R.id.icon4);
+        icon4.setImageResource(R.drawable.yacht);
+        ImageButton icon5 = (ImageButton) findViewById(R.id.icon5);
+        icon5.setImageResource(R.drawable.apartment);
+        ImageButton icon6 = (ImageButton) findViewById(R.id.icon6);
+        icon6.setImageResource(R.drawable.phone);
+        ImageButton icon7 = (ImageButton) findViewById(R.id.icon7);
+        icon7.setImageResource(R.drawable.shoppingbasket);
+        ImageButton icon8 = (ImageButton) findViewById(R.id.icon8);
+        icon8.setImageResource(R.drawable.yacht);
+        ImageButton icon9 = (ImageButton) findViewById(R.id.icon9);
+        icon9.setImageResource(R.drawable.apartment);
+        ImageButton icon10 = (ImageButton) findViewById(R.id.icon10);
+        icon10.setImageResource(R.drawable.yacht);
+        ImageButton icon11 = (ImageButton) findViewById(R.id.icon11);
+        icon11.setImageResource(R.drawable.apartment);
+        ImageButton icon12 = (ImageButton) findViewById(R.id.icon12);
+        icon12.setImageResource(R.drawable.apartment);
+
     }
 
     @Override
@@ -34,5 +64,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void openCategory(View v){
+        Intent intent = new Intent(getApplicationContext(), Category.class);
+        ImageButton icon = (ImageButton) findViewById(R.id.icon1);
+        String iconName = icon.getTag().toString();
+        intent.putExtra("name", iconName);
+        startActivity(intent);
     }
 }
