1b659e8e869963a2c51298356dbbd43b39dc3265
yoggli <Yannick.Baettig@bbcag.ch>
2015-06-30 16:38:45 +0200

    Navigation Drawer finished

--- app/src/main/java/headfirstandroiddevelopment/budgeteer/Overview.java
+++ app/src/main/java/headfirstandroiddevelopment/budgeteer/Overview.java
@@ -2,6 +2,7 @@ package headfirstandroiddevelopment.budgeteer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -23,20 +24,24 @@ import android.widget.Toast;
 import java.text.NumberFormat;
 
 
-public class Overview extends ActionBarActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class Overview extends BaseActivity {
 
     final static String MY_DB_NAME = "Budgeteer";
     final static String MY_DB_TABLE = "konto";
     final static String tag = "ensacom";
 
-    private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    private CharSequence mTitle;
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items); // load titles from strings.xml
+
+        navMenuIcons = getResources().obtainTypedArray(R.array.nav_drawer_icons);//load icons from strings.xml
+        set(navMenuTitles, navMenuIcons);
 
         getInputs();
 
@@ -45,27 +50,12 @@ public class Overview extends ActionBarActivity implements NavigationDrawerFragm
         dropDB();
         /*setContentView(R.layout.activity_main);*/
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.overview_activity));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.global, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.global, menu);
+        return true;
     }
 
     @Override
@@ -85,47 +75,48 @@ public class Overview extends ActionBarActivity implements NavigationDrawerFragm
 
     /** Datenbank und Tabellen erstellen wenn noch nicht vorhanden */
 
-    /** TODO: Testen ob Datenbankconnection funktioniert
-     *
+    /**
+     * TODO: Testen ob Datenbankconnection funktioniert
      */
-    private void onCreateDBAndDBTabled()
-    {
+    private void onCreateDBAndDBTabled() {
         SQLiteDatabase myDB = null;
         try {
             myDB = this.openOrCreateDatabase(MY_DB_NAME, MODE_PRIVATE, null);
             myDB.execSQL("CREATE TABLE IF NOT EXISTS " + MY_DB_TABLE
-                            + " (konto_id integer primary key autoincrement, datum integer(100), betrag real(100));");
+                    + " (konto_id integer primary key autoincrement, datum integer(100), betrag real(100));");
 
             Intent intent = getIntent();
             int day = intent.getIntExtra("day", 0);
             Double amount = 12.3; /* zum testen: später double amount */
              /*String category = intent.getStringExtra("name");*/
 
-            myDB.execSQL("INSERT INTO "+MY_DB_TABLE+" (datum, betrag) "
-                +"VALUES ('"+day+"',"+
-                "'"+amount+
-                "');");
+            myDB.execSQL("INSERT INTO " + MY_DB_TABLE + " (datum, betrag) "
+                    + "VALUES ('" + day + "'," +
+                    "'" + amount +
+                    "');");
             Log.v(tag, "Insert new Entry: " + day + ", " + amount);
-            } catch (Exception e){
-                Log.v(tag, e.getMessage());
-            }
+        } catch (Exception e) {
+            Log.v(tag, e.getMessage());
+        }
     }
-    public void dropDB(){
+
+    public void dropDB() {
 
 
     }
-    public void getInputs(){
+
+    public void getInputs() {
         Intent intent = getIntent();
         if (intent.hasExtra("amount")) {
             int day = intent.getIntExtra("day", 0);
             int month = intent.getIntExtra("month", 0);
             int year = intent.getIntExtra("year", 0);
-            String date = day + "." + month + "." +year;
+            String date = day + "." + month + "." + year;
 
             String category = intent.getStringExtra("name");
 
             NumberFormat defaultFormat = NumberFormat.getCurrencyInstance();
-            String message = String.valueOf( ".- am " + String.valueOf(day) + "." + String.valueOf(month) + "." + String.valueOf(year) + " in " + category + " gespeichert");
+            String message = String.valueOf(".- am " + String.valueOf(day) + "." + String.valueOf(month) + "." + String.valueOf(year) + " in " + category + " gespeichert");
 
             /** Dem User mitteilen, dass Eingabe gespeichert wurde */
             Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
@@ -135,81 +126,12 @@ public class Overview extends ActionBarActivity implements NavigationDrawerFragm
 
             /* Daten in Liste ausgeben */
             ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-            adapter.add("Übersicht: " +category);
-            adapter.add("Speicherdatum: "+date);
+            adapter.add("Übersicht: " + category);
+            adapter.add("Speicherdatum: " + date);
 
             ListView list = (ListView) findViewById(R.id.listoverview);
             list.setAdapter(adapter);
         }
     }
 
-
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.overview_container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
-
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_layout, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((Overview) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
 }
